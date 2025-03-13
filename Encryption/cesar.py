cba48670162f5d423c42fae6411dfcfd5577fd2b
pierre-antoine.fournier <paf.fournier@gmail.com>
2025-03-13 19:37:37 +0800

    add encryption and decryption

--- Encryption/cesar.py
+++ Encryption/cesar.py
@@ -1,7 +1,65 @@
-def encrypt(file, key="../key"):
+def key_int(key="../key") -> int:
     k = open(key, 'r')
+    key = k.readline()
+    res: int = 0
+    for char in key:
+        res *= 10
+        match char:
+            case '0':
+                res += 0
+            case '1':
+                res += 1
+            case '2':
+                res += 2
+            case '3':
+                res += 3
+            case '4':
+                res += 4
+            case '5':
+                res += 5
+            case '6':
+                res += 6
+            case '7':
+                res += 7
+            case '8':
+                res += 8
+            case '9':
+                res += 9
+    k.close()
+    res //=10
+    return res
+
+def encrypt(file: str, key: int) -> None:
     doc = open(file, 'r')
     new = open(file + ".cesar", 'w')
+    line: str = doc.readline()
+    while line:
+        for char in line:
+            if char == "\n":
+                new.write("\n")
+                continue
+            c: int = ord(char) + key % 256
+            new.write(chr(c))
+        line = doc.readline()
+    doc.close()
+    new.close()
+
+def decrypt(file: str, key: int, name: str) -> None:
+    doc = open(file, 'r')
+    new = open(name, 'w')
+    line: str = doc.readline()
+    while line:
+        for char in line:
+            if char == "\n":
+                new.write("\n")
+                continue
+            c: int = ord(char) - key % 256
+            new.write(chr(c))
+        line = doc.readline()
+    doc.close()
+    new.close()
 
 if __name__ == '__main__':
-    encrypt("../key")
+    encrypt("../key", key_int())
+    decrypt("../key.cesar", key_int(), "../key.decrypt")
+
